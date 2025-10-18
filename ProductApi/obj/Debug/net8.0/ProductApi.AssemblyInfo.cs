8290c277e0a47b8910891c39a6f0e7c62067759d
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-18 12:03:45 -0500

    commit - cuatro .

--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712ee0626465acf653f723cd1f1effaa5906476a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9a42b0793d3c8214980eee636babf4e01913b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
