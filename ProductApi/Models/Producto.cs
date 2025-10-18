1b58c104e153ca76e7d37cc23bebaf1f503d713c
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-18 12:14:07 -0500

    commit - cinco :.

--- ProductApi/Models/Producto.cs
+++ ProductApi/Models/Producto.cs
@@ -1,6 +1,27 @@
-﻿namespace ProductApi.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ProductApi.Models
 {
+
+    [Table("PRODUCTOS")]
     public class Producto
     {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+
+        [Column("PRODUCTO")]
+        [Required]
+        public string? Nombre { get; set; }
+
+        [Column("ESTADO_PRODUCTO")]
+        [Required]
+        public string? EstadoProducto { get; set; }
+
+        [Column("FECHA_TIMESTAMP")]
+        public DateTime FechaTimestamp { get; set; } = DateTime.Now;
     }
 }
