6faa6e00ecec656d7ed3d31c318b8991168c85b5
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-18 12:24:31 -0500

    commit - seis .

--- ProductApi/Data/OracleDbContext.cs
+++ ProductApi/Data/OracleDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace ProductApi.Data
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+
+namespace ProductApi.Data
 {
-    public class OracleDbContext
+    public class OracleDbContext : DbContext
     {
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos => Set<Producto>();
     }
 }
--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9a42b0793d3c8214980eee636babf4e01913b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b58c104e153ca76e7d37cc23bebaf1f503d713c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
